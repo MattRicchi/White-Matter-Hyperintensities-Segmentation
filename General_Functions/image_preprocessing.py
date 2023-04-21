3c15dc6656dae0cde9af5b28e6ef066ab1c503ca
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-04-21 12:34:58 +0200

    updated image_preprocessing.py

---
+++ General_Functions/image_preprocessing.py
@@ -0,0 +1,35 @@
+def imagePreProcessing(img, brain, label):
+    """
+    Function for the preprocessing of the images.
+  
+    It performs multiple tasks: conversion of the image in float32, crop image dimentions to (256x256)
+    and normalizes the image intensity by Gaussian normalization over the brain volume.
+  
+    Parameters
+    ----------    
+        img (Array): input image.
+        brain (Array): brain mask.
+        label (Array): label image.
+  
+    Returns
+    ----------
+        img (Array of float32): cropped and normalized image.
+        label (Array of float32): cropped label image.
+    """
+    import numpy as np
+
+    # Convert images in float32
+    img = np.float32(img)
+    brain = np.float32(brain)
+    label = np.float32(label)
+    
+    # Crop images to standard dimentions (256x256)
+    (image_rows, image_columns) = img.shape
+    standard_dimentions = 256
+    img = img[(int(image_rows/2) - int(standard_dimentions/2)) : (int(image_rows/2) + int(standard_dimentions/2)),
+              (int(image_columns/2) - int(standard_dimentions/2)) : (int(image_columns/2) + int(standard_dimentions/2))]
+    brain = brain[(int(image_rows/2) - int(standard_dimentions/2)) : (int(image_rows/2) + int(standard_dimentions/2)),
+                  (int(image_columns/2) - int(standard_dimentions/2)) : (int(image_columns/2) + int(standard_dimentions/2))]
+    label = label[(int(image_rows/2) - int(standard_dimentions/2)) : (int(image_rows/2) + int(standard_dimentions/2)),
+                  (int(image_columns/2) - int(standard_dimentions/2)) : (int(image_columns/2) + int(standard_dimentions/2))]
+    
