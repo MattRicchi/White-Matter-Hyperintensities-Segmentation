50b22696baf063db77289c191ebf43ccf78a42e8
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-05-11 17:19:26 +0200

    improved dice_coef_for_training

--- General_Functions/Training_Functions.py
+++ General_Functions/Training_Functions.py
@@ -30,7 +30,8 @@ def dice_coef_for_training(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    dice_coef = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return dice_coef
 
 def dice_coef_loss(y_true, y_pred):
     import numpy as np
