e6e2c14d177ff8213f1bb7e39a7d92d628fd42f5
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-05-11 17:22:59 +0200

    saveSlice corrected
    added the conversion to nii file with nib.Nifti1Image

--- General_Functions/Nii_Functions.py
+++ General_Functions/Nii_Functions.py
@@ -32,37 +32,15 @@ def saveSlice(img, fname, path):
         path (string): where to save the image.
     """
     import os
+    import numpy as np
     import nibabel as nib
     # First check if the path exists and if not make it
     if not os.path.exists(path):
         os.mkdir(path)
     # Then save img as .nii file
+    img = nib.Nifti1Image(img, np.eye(4))
     fout = os.path.join(path, f'{fname}.nii')
     nib.save(img, fout)
-
-    return
-
-def sliceAndSaveVolumeImage(vol, fname, path, SLICE_DECIMATE_IDENTIFIER = 3):
-    """
-    Save a volume image into single slices in the .nii format
-
-    Parameters
-    ----------
-        vol (array): volume to be saved into slices.
-        fname (string): name with which the slices will be saved.
-        path (string): where to save the slices.
-    """
-    import os
-    import nibabel as nib
-    import numpy as np
-    # First check if path exists and if not make it
-    if not os.path.exists(path):
-        os.mkdir(path)
-    # Get volume dimentions
-    (number_of_slices, image_rows, image_columns, channel_number) = vol.shape
-    # Save volume image into single slices
-    for i in range(number_of_slices):
-        saveSlice(nib.Nifti1Image(vol[i, :, :, 0], np.eye(4)), f'{fname}', path) 
 
     return
     
