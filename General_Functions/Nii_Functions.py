994c26bdf46337a763763ddc3f209fcf70959068
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-05-12 16:27:16 +0200

    Added documentation to the functions

--- General_Functions/Nii_Functions.py
+++ General_Functions/Nii_Functions.py
@@ -7,66 +7,182 @@ Date: May 2023
 
 def readImage(imgPath):
     """
-    Function to read and load a .nii file
+    Reads in a medical image from a file and returns its data as a NumPy array.
 
-    Parameters
-    ----------
-        imgPath (string): Path of the .nii file to be read and loaded . 
+    Parameters:
+    -----------
+    imgPath : str
+        The file path of the medical image to be read.
 
-    Returns
+    Returns:
+    --------
+    image : ndarray
+        A NumPy array containing the data of the medical image.
+    
+    Raises:
     -------
-        (array): File located at imgPath.
+    FileNotFoundError
+        If the specified file path does not exist.
+    nibabel.filebasedimages.ImageFileError
+        If the specified file path is not a valid medical image file.
+
+    Example:
+    ---------
+    >>> readImage('example.nii.gz')
+    array([[[ 0.11,  0.22,  0.33],
+            [ 0.44,  0.55,  0.66]],
+
+           [[ 0.77,  0.88,  0.99],
+            [ 1.11,  1.22,  1.33]]])
     """
     import nibabel as nib
-    image = nib.load(imgPath).get_fdata()
-    return image
+    
+    try:
+        # Load the image using nibabel and retrieve its data
+        image = nib.load(imgPath).get_fdata()
+        return image
+    except FileNotFoundError:
+        # Raise an error if the file path does not exist
+        raise FileNotFoundError(f"File not found: {imgPath}")
+    except nib.filebasedimages.ImageFileError:
+        # Raise an error if the file path is not a valid medical image file
+        raise nib.filebasedimages.ImageFileError(f"Not a valid medical image file: {imgPath}")
+
 
 def saveSlice(img, fname, path):
     """
-    Save an image in the .nii format
+    Save an image in the NIfTI format (.nii file extension).
 
     Parameters
     ----------
-        img (array): image to be saved.
-        fname (string): name with which the image will be saved.
-        path (string): where to save the image.
+    img : array-like
+        The image data to be saved. This can be a NumPy array or any other
+        array-like object that can be converted to a NumPy array.
+    fname : str
+        The file name to be used for saving the image, without the file
+        extension. For example, if `fname` is "image" then the saved file
+        will be named "image.nii".
+    path : str
+        The directory path where the image file will be saved. If the path
+        does not exist, it will be created.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    TypeError
+        If `img` is not an array-like object.
+    ValueError
+        If `fname` is not a string or if it is an empty string.
+        If `path` is not a string or if it is an empty string.
+    OSError
+        If the file could not be saved for any reason (e.g., the output
+        file already exists and cannot be overwritten, or the output
+        directory cannot be created).
+
+    Example
+    --------
+    >>> import numpy as np
+    >>> img = np.random.rand(10, 10, 10)
+    >>> saveSlice(img, "image", "/path/to/output/dir")
     """
     import os
     import numpy as np
     import nibabel as nib
-    # First check if the path exists and if not make it
+
+    # Check inputs for correctness
+    if not isinstance(img, np.ndarray):
+        raise TypeError("Expected `img` to be an array-like object.")
+
+    if not isinstance(fname, str) or not fname:
+        raise ValueError("`fname` must be a non-empty string.")
+
+    if not isinstance(path, str) or not path:
+        raise ValueError("`path` must be a non-empty string.")
+
+    # Create output directory if it does not exist
     if not os.path.exists(path):
         os.mkdir(path)
-    # Then save img as .nii file
-    img = nib.Nifti1Image(img, np.eye(4))
-    fout = os.path.join(path, f'{fname}.nii')
-    nib.save(img, fout)
+
+    # Save image to file
+    try:
+        img = nib.Nifti1Image(img, np.eye(4))
+        fout = os.path.join(path, f"{fname}.nii")
+        nib.save(img, fout)
+    except Exception as e:
+        raise OSError(f"Failed to save image to file: {e}")
 
     return
+
     
 def concatenateImages(flair_img, t1w_img):
     """
-    Concatenate two 2D images into one 3D image.
+    Concatenate two 2D images along a new channel axis to create a 3D image.
 
     Parameters
     ----------
-        flair_img (array): first image to be concatenated.
-        t1w_img (array): second image to be concatenated.
-    
+    flair_img : array-like
+        A NumPy array containing the first 2D image to be concatenated.
+    t1w_img : array-like
+        A NumPy array containing the second 2D image to be concatenated.
+
     Returns
     -------
-        FLAIR_and_T1W_image (array): 3D image containing flair_img and t1w_img.
+    FLAIR_and_T1W_image : ndarray
+        A 3D NumPy array containing the two 2D images concatenated along the third dimension.
+
+    Raises
+    ------
+    TypeError 
+        If either flair_img or t1w_img is not a numpy.ndarray.
+    ValueError
+        If the shape of the two input images is not the same.
+        If either flair_img or t1w_img does not have exactly two dimensions.
+
+    Notes
+    -----
+    The input images must be 2D and have the same shape.
+
+    Example
+    --------
+    >>> flair_img = np.array([[1, 2], [3, 4]])
+    >>> t1w_img = np.array([[5, 6], [7, 8]])
+    >>> concatenateImages(flair_img, t1w_img)
+    array([[[1., 5.],
+            [2., 6.]],
+
+           [[3., 7.],
+            [4., 8.]]], dtype=float32)
+
     """
     import numpy as np
+
+    # Check inputs for correctness
+    if not isinstance(flair_img, np.ndarray) or not isinstance(t1w_img, np.ndarray):
+        raise TypeError("Inputs must be numpy arrays.")
+    
+    if flair_img.shape != t1w_img.shape:
+        raise ValueError("Input images must have the same shape.")
+    
+    if flair_img.ndim != 2 or t1w_img.ndim != 2:
+        raise ValueError("Inputs must be 2D numpy arrays.")
+
     # Get the shape of the two images
     (image_rows, image_columns) = flair_img.shape
-    # Define the channel on which the two images will be concatenated
+
+    # Define the dimention of the channel on which the two images will be concatenated
     channel_number = 2
+
     # Define the final 3D image
-    FLAIR_and_T1W_image = np.ndarray((image_rows, image_columns, channel_number), dtype = np.float32)
+    FLAIR_and_T1W_image = np.ndarray((image_rows, image_columns, channel_number), dtype=np.float32)
+
     # Create a new axis in the two 2D images
     flair_img = flair_img[..., np.newaxis]
     t1w_img = t1w_img[..., np.newaxis]
+
     # Finally, concatenate the 2D images into a 3D image
-    FLAIR_and_T1W_image = np.concatenate((flair_img, t1w_img), axis = 2)
+    FLAIR_and_T1W_image = np.concatenate((flair_img, t1w_img), axis=2)
+
     return FLAIR_and_T1W_image
--- General_Functions/image_preprocessing.py
+++ General_Functions/image_preprocessing.py
@@ -4,23 +4,112 @@ This script contains all the necessary functions for the preprocessing of the MR
 Author: Mattia Ricchi
 Date: May 2023
 '''
-def crop_image(image, standard_dimentions = 256):
-    '''
-    Function to crop the input image to standard dimentions (256x256)
-    '''
+def crop_image(image, standard_dimensions=256):
+    """
+    Crop a given 2D image to a specified size around its center.
+
+    Parameters
+    ----------
+    image : numpy.ndarray
+        The 2D image to be cropped.
+    standard_dimensions : int, optional
+        The desired size of the cropped image. Default is 256.
+
+    Returns
+    -------
+    cropped_image : numpy.ndarray
+        The cropped image.
+
+    Raises
+    ------
+    TypeError
+        If the input image is not a numpy array.
+    ValueError
+        If the input image is not a 2D array.
+        If the dimensions of the input image are smaller than the specified crop size.
+
+    Notes
+    -----
+    This function crops the image to the specified size around its center, such that the cropped image has
+    the same number of rows and columns as the specified crop size.
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> image = np.random.rand(512, 512)
+    >>> cropped_image = crop_image(image, 256)
+    >>> cropped_image.shape
+    (256, 256)
+    """
+    import numpy as np
+
     (image_rows, image_columns) = image.shape
 
-    cropped_image = image[(int(image_rows/2) - int(standard_dimentions/2)) : (int(image_rows/2) + int(standard_dimentions/2)),
-              (int(image_columns/2) - int(standard_dimentions/2)) : (int(image_columns/2) + int(standard_dimentions/2))]
+    # Check inputs for correctness
+    if not isinstance(image, np.ndarray):
+        raise TypeError("Input image must be a numpy array.")
+    if not image.ndim == 2:
+        raise ValueError("Input image must be a 2D array.")
+
+    # Check that the dimensions of the input image are not smaller than the crop size
+    if image_rows < standard_dimensions or image_columns < standard_dimensions:
+        raise ValueError("Image dimensions are smaller than the specified crop size.")
+
+    # Crop the input image to have dimensions match the specified crop size
+    cropped_image = image[(int(image_rows/2) - int(standard_dimensions/2)) : (int(image_rows/2) + int(standard_dimensions/2)),
+              (int(image_columns/2) - int(standard_dimensions/2)) : (int(image_columns/2) + int(standard_dimensions/2))]
 
     return cropped_image
 
 
 def gaussian_normalisation(image, brain_mask):
-    '''
-    Function to perform gaussian normalisation of pixel intensity
-    '''
+    """
+    Apply Gaussian normalization to an image using the mean and standard deviation within a brain mask.
+
+    Parameters:
+    ----------
+    image : np.ndarray
+        The input image to be normalized.
+    brain_mask : np.ndarray
+        A binary mask indicating the region of the brain in the image.
+
+    Returns:
+    -------
+    np.ndarray
+        The normalized image.
+
+    Raises:
+    -------
+    TypeError 
+        If either image or brain_mask is not a numpy.ndarray.
+    ValueError
+        If the shapes of the image and brain_mask arrays do not match.
+
+    Notes:
+    -----
+    Gaussian normalization is a technique commonly used in medical imaging to adjust the intensity of images to have a
+    zero mean and unit variance. This is done to reduce the impact of intensity variations due to scanner artifacts
+    and other factors.
+
+    Example:
+    -------
+    >>> image = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+    >>> brain_mask = np.array([[0, 0, 0], [1, 1, 1], [1, 1, 1]])
+    >>> gaussian_normalisation(image, brain_mask)
+    array([[-1.22474487, -1.22474487, -1.22474487],
+           [ 0.        ,  0.        ,  0.        ],
+           [ 1.22474487,  1.22474487,  1.22474487]])
+    """
+    
     import numpy as np
+    
+    # Check inputs for correctness
+    if not isinstance(image, np.ndarray) or not isinstance(brain_mask, np.ndarray):
+        raise TypeError("Inputs must be numpy arrays.")
+
+    # Check that the shape of the image and brain_mask match
+    if image.shape != brain_mask.shape:
+        raise ValueError("The shapes of the input image and brain mask do not match.")
     
     # Compute the mean and standard deviation of the image within the brain mask
     image_mean = np.mean(image[brain_mask == 1.0])
@@ -32,45 +121,113 @@ def gaussian_normalisation(image, brain_mask):
     return normalised_image
 
 
+
 def float32_converter(image):
-    '''
-    Function to convert the input image to numpy float32 array
-    '''
+    """
+    Converts the input image to a float32 numpy array.
+
+    Parameters
+    ----------
+    image : ndarray
+        Input image as a numpy array.
+
+    Returns
+    -------
+    ndarray
+        Float32 numpy array.
+
+    Raises
+    ------
+    TypeError
+        If the input image is not a numpy array.
+
+    Notes
+    -----
+    The float32 numpy array is useful for numerical calculations as it provides greater precision than other data types.
+    If the input image is already a float32 numpy array, the function returns the same array without any conversion.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> img = np.array([[1, 2], [3, 4]])
+    >>> float_img = float32_converter(img)
+    >>> float_img
+    array([[1., 2.],
+           [3., 4.]], dtype=float32)
+    """
     import numpy as np
+
+    if not isinstance(image, np.ndarray):
+        raise TypeError("Input image must be a numpy array.")
+
     image_float32 = np.float32(image)
 
     return image_float32
 
 
+
 def imagePreProcessing(image, brain_mask, label):
     """
-    Function for the preprocessing of the images:
-    This function converts the input images into float32, crops images dimentions to (256x256)
-    and normalizes the brain image intensity by Gaussian normalization over the brain volume.
+    Applies a series of preprocessing steps to an input image, brain mask, and label.
 
-    Parameters
-    ----------    
-        image (Array): input brain image.
-        brain_mask (Array): brain mask.
-        label (Array): label image.
-  
-    Returns
-    ----------
-        image (Array of float32): cropped and normalized brain image.
-        label (Array of float32): cropped label image.
+    Parameters:
+    -----------
+    image : numpy.ndarray
+        The input image to be preprocessed.
+    brain_mask : numpy.ndarray
+        A mask of the brain volume within the input image.
+    label : numpy.ndarray
+        The label for the input image.
+
+    Returns:
+    --------
+    Tuple[numpy.ndarray, numpy.ndarray]
+        A tuple containing the preprocessed image and label.
+
+    Raises:
+    -------
+    TypeError
+        If the input image, brain mask, or label is not a numpy array.
+    ValueError
+        If the input image, brain mask, or label is not a 2D array.
+        If the input image, brain mask, or label does not have the same shape.
+
+    Notes:
+    ------
+    This function applies the following preprocessing steps to the input image and mask:
+    1. Converts the input image, brain mask, and label to a float32 data type.
+    2. Crops the image, brain mask, and label to standard dimensions of 256x256.
+    3. Normalizes the image using a Gaussian distribution with zero mean and unit variance over the brain volume.
+
+    Example:
+    --------
+    >>> image = np.random.rand(176, 256, 256)
+    >>> brain_mask = np.random.rand(176, 256, 256)
+    >>> label = np.random.rand(176, 256, 256)
+    >>> preprocessed_image, preprocessed_label = imagePreProcessing(image, brain_mask, label)
     """
+    import numpy as np
+    from General_Functions.image_preprocessing import float32_converter, crop_image, gaussian_normalisation
 
-    # Convert images in float32
+    # Check inputs are numpy arrays and have the correct dimensions
+    if not all(isinstance(arr, np.ndarray) for arr in [image, brain_mask, label]):
+        raise TypeError("Input image, brain mask, and label must be numpy arrays.")
+    if not all(arr.ndim == 2 for arr in [image, brain_mask, label]):
+        raise ValueError("Input image, brain mask, and label must be 3D arrays.")
+    if not all(arr.shape == image.shape for arr in [brain_mask, label]):
+        raise ValueError("Input image, brain mask, and label must have the same shape.")
+
+    # Convert images to float32
     image = float32_converter(image)
     brain_mask = float32_converter(brain_mask)
     label = float32_converter(label)
 
-    # Crop images to standard dimentions (256x256)
+    # Crop images to standard dimensions (256x256)
     image = crop_image(image)
     brain_mask = crop_image(brain_mask)
     label = crop_image(label)
 
     # Gaussian normalization over brain volume
     image = gaussian_normalisation(image, brain_mask)
-    
+
     return image, label
