1de19b490d9be01cb3ebb39c907a5817432b39c7
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-05-30 17:36:14 +0200

    Added postProcessing function

--- General_Functions/postprocessing.py
+++ General_Functions/postprocessing.py
@@ -53,3 +53,63 @@ def get_evaluation_metrics(true_image, predicted_image):
     f1 = f1_score(true_image, predicted_image, average='micro', zero_division=True)
     
     return dsc, precision, recall, f1
+
+
+def imagePostProcessing(img, label):
+    """
+    Function for the postprocessing of the images:
+    The function converts the input images to float32 data type;
+    It then crops the images to the standard dimensions of 256x256;
+    The image intensity is normalized using the cv2.normalize function with alpha=0 and beta=255.
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        Input image.
+    label : numpy.ndarray
+        Label image.
+
+    Returns
+    -------
+    numpy.ndarray
+        Cropped and normalized image.
+    numpy.ndarray
+        Cropped label image.
+
+    Notes
+    -----
+    The function converts the input images to float32 data type.
+    It then crops the images to the standard dimensions of 256x256.
+    The image intensity is normalized using the cv2.normalize function with alpha=0 and beta=255.
+    The resulting image and label are returned as a tuple.
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> from image_processing import imagePreProcessing
+
+    >>> img = np.random.rand(512, 512)  # Sample input image
+    >>> label = np.random.randint(0, 2, (512, 512))  # Sample label image
+
+    >>> processed_img, processed_label = imagePreProcessing(img, label)
+    >>> print(processed_img.shape)
+    (256, 256)
+    >>> print(processed_label.shape)
+    (256, 256)
+    """
+    import numpy as np
+    import cv2
+    from General_Functions.image_preprocessing import float32_converter, crop_image
+
+    # Convert images to float32
+    img = float32_converter(img)
+    label = float32_converter(label)
+
+    # Crop to standard dimensions (256x256)
+    img = crop_image(img)
+    label = crop_image(label)
+
+    # Normalize image intensity 0 to 255
+    img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+    return img, label
