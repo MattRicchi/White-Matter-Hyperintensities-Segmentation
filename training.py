c115de0dfacd791c65a3392a96792190f33d41c1
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-06-05 15:07:25 +0200

    Comments quality improved

--- training.py
+++ training.py
@@ -2,10 +2,10 @@
 
 from setup import Setup_Script
 
-# Check everything is correctly setted before running the script
+# Check everything is correctly set before running the script
 Setup_Script()
 
-# Import necessary packages
+# Import necessary packages and modules
 import os
 import time
 import nibabel as nib
@@ -19,7 +19,7 @@ from keras.callbacks import ModelCheckpoint
 from keras import backend as K
 from focal_loss import BinaryFocalLoss, binary_focal_loss
 
-# Import necessary functions
+# Import necessary functions and modules
 from unet import get_unet
 from General_Functions.Training_Functions import dice_coef_loss, scheduler, dataAugmentation
 from General_Functions.image_preprocessing import imagePreProcessing
@@ -27,7 +27,7 @@ from General_Functions.Nii_Functions import readImage, concatenateImages, saveSl
 
 Total_Start = time.time()
 
-# Define all the necessary paths
+# Set up necessary paths for data, results, and weights
 data_path = join(os.getcwd(), 'DATABASE')
 results_path = join(os.getcwd(), 'Results') # Folder where all result images will be saved
 flair_path = join(data_path, 'OnlyBrain/flair/')
@@ -45,7 +45,7 @@ image_shape = (256, 256, 2)
 # This will be passed as an input to the three models of the ensemble
 inputs = L.Input(shape=image_shape)
 
-print('Loading and compiling the three models of the ensemble')
+print('Loading and compiling the three models of the ensemble.')
 
 # First model of the ensemble
 model0 = get_unet(inputs)
@@ -59,7 +59,7 @@ _ = model1.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-6), loss=
 model2 = get_unet(inputs)
 _ = model2.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5), loss=BinaryFocalLoss(gamma=2), metrics=[binary_focal_loss])
 
-# Visually check that the model is correctly compiled
+# Print model summaries for an overview of the architectures to check that they are correctly compiled
 model0.summary()
 model1.summary()
 model2.summary()
@@ -84,22 +84,26 @@ Image_IDs = np.empty(0)
 # Iterate over the image IDs and label the images as Train or Test image
 print('Building X_train, Y_train and X_test... ')
 for n, id_ in tqdm(enumerate(image_ids), total=len(image_ids)):
+    # Load the images and labels
     flair_image = readImage(join(flair_path, f'{id_}'))
     t1w_image = readImage(join(t1w_path, f'{id_}'))
     label_image = readImage(join(label_path, f'{id_}'))
     brain_mask = readImage(join(brain_path, f'{id_}'))
 
+    # Preprocess the images and labels
     (flair_image, label_image) = imagePreProcessing(flair_image, brain_mask, label_image)
     (t1w_image, label_image) = imagePreProcessing(t1w_image, brain_mask, label_image)
     
+    # Skip the images in which there is no brain
     if np.max(brain_mask) == 0.0:
-        continue # Skip the images in which there is no brain
+        continue 
 
+    # Concatenate FLAIR and T1W images
     FLAIR_and_T1W_image = concatenateImages(flair_image, t1w_image)
     FLAIR_and_T1W_image = FLAIR_and_T1W_image[np.newaxis, ...]
     label_image = label_image[np.newaxis, ..., np.newaxis]
     
-    # Sort images out basing on the patient number
+    # Sort images based on the patient number
     patient_number = int(id_[7:10])
     if patient_number in test_patients:
         # Image will be used for testing
@@ -111,7 +115,7 @@ for n, id_ in tqdm(enumerate(image_ids), total=len(image_ids)):
         TRAIN_IMAGES = np.append(TRAIN_IMAGES, FLAIR_and_T1W_image, axis = 0)
         TRAIN_LABELS = np.append(TRAIN_LABELS, label_image, axis = 0)
         
-        # If there are labelled lesions in the slice, apply data augmentation 10 times
+        # Apply data augmentation 10 times if there are labeled lesions in the slice
         if id_[:14] in labeled_ids:
             labelImg = label_image[0, :, :, 0]
             for k in range(0, 9):
@@ -123,7 +127,7 @@ for n, id_ in tqdm(enumerate(image_ids), total=len(image_ids)):
                 TRAIN_LABELS = np.append(TRAIN_LABELS, labelAug, axis = 0)
                 k += 1
 
-# Fit the model
+# Fit the models
 print('Starting to fit the models in the enseble, will take a while...')
 
 start = time.time()
@@ -134,16 +138,17 @@ _ = model2.fit(TRAIN_IMAGES, TRAIN_LABELS, validation_split = 0.1, batch_size = 
 
 end = time.time()
 
+# Print a message indicating the completion of the training
+print('Training completed successfully!')
 print('Time spent training the network: ',(end - start)/(60*60), ' hours')
 
 print('Starting to test the network...')
 
 start = time.time()
 
+# Test the three models of the ensemble
 preds_test_0 = model0.predict(TEST_IMAGES, verbose = 1)
-
 preds_test_1 = model1.predict(TEST_IMAGES, verbose = 1)
-
 preds_test_2 = model2.predict(TEST_IMAGES, verbose = 1)
 
 # Average the results from the three models of the ensamble
@@ -165,7 +170,8 @@ for i in tqdm(range(preds_test_t.shape[0])):
     slice_number = int(patient_name[11:14])
     saveSlice(nib.Nifti1Image(preds_test_t[i, :, :, 0], np.eye(4)), f'volume-{str(patient_number).zfill(SLICE_DECIMATE_IDENTIFIER)}-{str(slice_number).zfill(SLICE_DECIMATE_IDENTIFIER)}', results_path)
     
-# Save the weights of the trained network
+# Save the final weights of the models
+print('Saving the final weights of the models...')
 model0.save_weights(join(weights_path, 'model0.h5'))
 model1.save_weights(join(weights_path, 'model1.h5'))
 model2.save_weights(join(weights_path, 'model2.h5'))
