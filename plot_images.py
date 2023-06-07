48f85f193b6bfc0c63ea7b0345724ac03262a106
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-06-07 12:55:04 +0200

    The script now simple calls the plot_images function

--- plot_images.py
+++ plot_images.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-This script is for plotting 
+This script is for plotting flair, ground truth and result images
 
 Author: Mattia Ricchi
 Date: May 2023
@@ -12,50 +12,7 @@ from setup import Setup_Script
 Setup_Script()
 
 # Import necessary functions and modules
-import os
-from os.path import join
-import matplotlib.pyplot as plt 
-from General_Functions.Nii_Functions import readImage
-from General_Functions.postprocessing import imagePostProcessing
+from General_Functions.plot_functions import plot_images
 
-# Set up necessary paths for data and results
-data_path = join(os.getcwd(), 'DATABASE')
-results_path = join(os.getcwd(), 'Results')
-flair_path = join(data_path, 'flair/')
-label_path = join(data_path, 'label/')
-
-# Set the name of the image you want to display
-id_ = 'volume-038-147.nii'
-volume_number = int(id_[7:10])
-slice_number = int(id_[11:14])
-
-# Read flair, label and result images
-flair_image = readImage(join(flair_path, id_))
-label = readImage(join(label_path, id_))
-result = readImage(join(results_path, id_))
-
-# Process the flair and ground truth images
-flair_image, label = imagePostProcessing(flair_image, label)
-
-# Define figure size and title
-fig = plt.figure(figsize=(15, 6))
-plt.title(f'Volume {volume_number} - Slice {slice_number}', fontsize = 25)
-plt.axis('off')
-
-rows = 1
-columns = 3
-
-# Add flair image at the 1st position
-fig.add_subplot(rows, columns, 1)
-plt.imshow(flair_image.T, cmap = 'gray')
-plt.title("Flair Image", fontsize = 16)
-
-# Add ground truth image at the 2nd position
-fig.add_subplot(rows, columns, 2)
-plt.imshow(label.T, cmap = 'gray')
-plt.title("Label Image", fontsize = 18)
-
-# Add segmentation result at the 3rd position
-fig.add_subplot(rows, columns, 3)
-plt.imshow(result.T, cmap = 'gray')
-plt.title("Segmantation Result", fontsize = 18)
+# Read and plot the images
+plot_images(38, 147)
