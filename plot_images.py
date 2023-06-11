9be1abf52b1fbd4408a6db8d2b2d732d495430c1
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-06-11 11:07:55 +0200

    Now asks patient and slice numbers from keyboard

--- plot_images.py
+++ plot_images.py
@@ -9,5 +9,9 @@ Date: May 2023
 # Import necessary functions and modules
 from General_Functions.plot_functions import plot_images
 
+# Get patient number and slice number to plot
+patient_number = input('Enter patient number: ')
+slice_number = input('Enter slice number: ')
+
 # Read and plot the images
-plot_images(38, 147)
+plot_images(patient_number, slice_number)
