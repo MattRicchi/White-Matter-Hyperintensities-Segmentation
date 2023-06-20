c5a2c1edc93e6433a54512489dd7d2be181a856d
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-06-20 16:10:27 +0200

    plot_images.py now uses sys and getopt

--- plot_images.py
+++ plot_images.py
@@ -6,13 +6,41 @@ Author: Mattia Ricchi
 Date: May 2023
 '''
 
-# Import necessary functions and modules
 import sys
+import getopt
 from General_Functions.plot_functions import plot_images
 
-# Get patient number and slice number to plot
-patient_number = sys.argv[1]
-slice_number = sys.argv[2]
+def main(argv):
+    patient_number = None
+    slice_number = None
 
-# Read and plot the images
-plot_images(patient_number, slice_number)
+    try:
+        opts, args = getopt.getopt(argv, "hp:s:", ["patient=", "slice="])
+    except getopt.GetoptError:
+        print('plot_images.py -p <patient_number> -s <slice_number>')
+        sys.exit(2)
+
+    # Parse the command-line options and arguments
+    for opt, arg in opts:
+        if opt == '-h':
+            # Display help message and exit
+            print('plot_images.py -p <patient_number> -s <slice_number>')
+            sys.exit()
+        elif opt in ("-p", "--patient"):
+            # Store the patient number
+            patient_number = arg
+        elif opt in ("-s", "--slice"):
+            # Store the slice number
+            slice_number = arg
+
+    # Check if both patient number and slice number are provided
+    if patient_number is None or slice_number is None:
+        print('Please provide both patient number and slice number.')
+        sys.exit(2)
+
+    # Call the plot_images function with the provided patient and slice numbers
+    plot_images(patient_number, slice_number)
+
+if __name__ == "__main__":
+    # Call the main function with the command-line arguments, excluding the script name
+    main(sys.argv[1:])
