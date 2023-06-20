94c7486fa86c366857b0f2435c2ce5c740c71b81
MattRicchi <mattia.ricchi3@studio.unibo.it>
2023-06-20 15:45:01 +0200

    Now using sys to get patient and slice numbers

--- plot_images.py
+++ plot_images.py
@@ -7,11 +7,12 @@ Date: May 2023
 '''
 
 # Import necessary functions and modules
+import sys
 from General_Functions.plot_functions import plot_images
 
 # Get patient number and slice number to plot
-patient_number = input('Enter patient number: ')
-slice_number = input('Enter slice number: ')
+patient_number = sys.argv[1]
+slice_number = sys.argv[2]
 
 # Read and plot the images
 plot_images(patient_number, slice_number)
